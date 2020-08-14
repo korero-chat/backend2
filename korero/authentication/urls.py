37f3007792de92b1b4802c35a3887aeb23004ef7
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-08-14 17:32:24 +0200

    Include views in urls.py

---
+++ korero/authentication/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from authentication import views
+
+
+app_name = 'authentication'
+urlpatterns = [
+    path('retrieve/<int:pk>', views.RetrieveUserView.as_view()),
+    path('edit/<int:pk>', views.EditUserView.as_view()),
+]
