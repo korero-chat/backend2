63558bf60ed3b264bbf98be7ffd150b3bf34d0a0
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-08-14 17:53:35 +0200

    Add create() method to UserSerializer

--- korero/authentication/serializers.py
+++ korero/authentication/serializers.py
@@ -6,3 +6,10 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = get_user_model()
         exclude = ['password', 'email', 'is_superuser', 'user_permissions']
+    
+    def create(self, validated_data):
+        user = get_user_model()(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
