171702d4b346259a41f45edac87bdcf666473424
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-08-14 18:21:55 +0200

    Give names to the authentication urls

--- korero/authentication/urls.py
+++ korero/authentication/urls.py
@@ -5,7 +5,8 @@ from authentication import views
 
 app_name = 'authentication'
 urlpatterns = [
-    path('retrieve/<int:pk>', views.RetrieveUserView.as_view()),
-    path('edit/<int:pk>', views.EditUserView.as_view()),
-    path('create/', views.CreateUserView.as_view()),
+    path('retrieve/<int:pk>',
+         views.RetrieveUserView.as_view(), name='retrieve'),
+    path('edit/<int:pk>', views.EditUserView.as_view(), name='edit'),
+    path('create/', views.CreateUserView.as_view(), name='create'),
 ]
