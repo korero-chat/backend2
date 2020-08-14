ab1b823f3f62ec9dc881e916dbebc4125fb27777
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-08-14 17:22:45 +0200

    Create user views

--- korero/authentication/views.py
+++ korero/authentication/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.generics import RetrieveAPIView, \
+                                    RetrieveUpdateDestroyAPIView
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from authentication.serializers import UserSerializer
+
+
+class RetrieveUserView(RetrieveAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class EditUserView(RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
