998612fd4fda222240621a47279c0d00ebe5ab3f
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-08-14 18:04:56 +0200

    Change UserSerializer fields, make some write-only

--- korero/authentication/serializers.py
+++ korero/authentication/serializers.py
@@ -1,11 +1,18 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from django.contrib.auth import get_user_model
 
 
-class UserSerializer(ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        validators=[UniqueValidator(queryset=get_user_model().objects.all())]
+    )
+    email = serializers.EmailField(write_only=True)
+
     class Meta:
         model = get_user_model()
-        exclude = ['password', 'email', 'is_superuser', 'user_permissions']
+        fields = ['username', 'email', 'password']
     
     def create(self, validated_data):
         user = get_user_model()(**validated_data)
