ff69759e7e3b4980ac5722a07952ab4ed1a220a0
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-08-14 18:05:10 +0200

    Add CreateUserView

--- korero/authentication/urls.py
+++ korero/authentication/urls.py
@@ -7,4 +7,5 @@ app_name = 'authentication'
 urlpatterns = [
     path('retrieve/<int:pk>', views.RetrieveUserView.as_view()),
     path('edit/<int:pk>', views.EditUserView.as_view()),
+    path('create/', views.CreateUserView.as_view()),
 ]
--- korero/authentication/views.py
+++ korero/authentication/views.py
@@ -1,18 +1,22 @@
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.generics import RetrieveAPIView, \
-                                    RetrieveUpdateDestroyAPIView
+from rest_framework import generics
 from django.contrib.auth import get_user_model
 
 from authentication.serializers import UserSerializer
 
 
-class RetrieveUserView(RetrieveAPIView):
+class CreateUserView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class RetrieveUserView(generics.RetrieveAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
 
-class EditUserView(RetrieveUpdateDestroyAPIView):
+class EditUserView(generics.RetrieveUpdateDestroyAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAdminUser]
